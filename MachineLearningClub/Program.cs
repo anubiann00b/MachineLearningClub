5aa18d467eae4a1dcc2847dffd571d1f483473e4
Shreyas <skraman1999@gmail.com>
2014-10-02 20:56:17 -0700

    Update Program.cs

--- MachineLearningClub/Program.cs
+++ MachineLearningClub/Program.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// COME TO MACHINE LEARNING CLUB IN O'BYRNE/THOMPSON ROOM ON
+// TUESDAYS AND THURSDAYS.
+
 namespace MachineLearningClub
 {
     /**
