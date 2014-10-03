4cc0d93e9b11f3d146867c7ecc7dd01925f74e84
Shreyas <skraman1999@gmail.com>
2014-10-02 20:24:09 -0700

    Comments!

--- MachineLearningClub/Program.cs
+++ MachineLearningClub/Program.cs
@@ -7,24 +7,31 @@ using System.Threading.Tasks;
 
 namespace MachineLearningClub
 {
+    /**
+    * Simple example of a basic perceptron.
+    */
     class PerceptronTester
     {
         static void Main(string[] args)
         {
-            // These test cases suck btw
+            // Test cases (that are really bad).
             double[][] input = new double[][]
             {
+                // Each array is a test case with 'length' dimensions
                 new double[]{1,1,0,1,1,0,1,0,1},
                 new double[]{0,0,1,0,1,1,1,1,1},
             };
+
+            // The expected output for each test case.
             double[] output = new double[] { 1, 0 };
 
-            PerceptronNetwork network = new PerceptronNetwork(9);
+            PerceptronNetwork network = new PerceptronNetwork(input[0].Length);
             PerceptronTeacher teacher = new PerceptronTeacher(network, 0.05D);
 
+            // Run the training 1000 times, since we don't have enough test cases.
             for (int x = 0; x < 1000; x++)
             {
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < output.Length; i++)
                 {
                     double error = teacher.Teach(input[i], output[i]);
                     Console.WriteLine(error);
@@ -36,9 +43,13 @@ namespace MachineLearningClub
 
     public class PerceptronNetwork
     {
+        // The number of inputs (dimension).
         public readonly int Inputs;
+
+        // The weight for each input, from -1 to 1.
         private double[] weights;
 
+        // Allows us to do C# magic, like PerceptronNetwork[i].
         public double this[int index]
         {
             get { return this.weights[index]; }
@@ -50,6 +61,8 @@ namespace MachineLearningClub
         public PerceptronNetwork(int inputs)
         {
             this.Inputs = inputs;
+
+            // Get an array of random values from -1 to 1.
             this.weights = Enumerable.Range(0, this.Inputs)
                 .Select(x => random.NextDouble() * 2 - 1)
                 .ToArray();
@@ -57,12 +70,16 @@ namespace MachineLearningClub
 
         public double Run(double[] inp)
         {
+            // Make sure the user isn't an idiot.
             Trace.Assert(weights.Length == inp.Length);
+
+            // Get the sum of each input multiplied by its corresponding weight.
             double sum = Enumerable
                 .Range(0, weights.Length)
                 .Select(x => weights[x] * inp[x])
                 .Sum();
 
+            // Simple hedge (aka step) function.
             if (sum > 0)
             {
                 return 1;
@@ -74,6 +91,8 @@ namespace MachineLearningClub
     public class PerceptronTeacher
     {
         public PerceptronNetwork Network { get; private set; }
+
+        // Constant representing training speed.
         public double Alpha { get; set; }
 
         public PerceptronTeacher(PerceptronNetwork network, double alpha)
@@ -84,13 +103,18 @@ namespace MachineLearningClub
 
         public double Teach(double[] input, double output)
         {
+            // Keep track of the total error. Should be converging to a small value.
             double sum = 0;
-            double a = output - this.Network.Run(input);
 
+            // Error = Expected - Actual
+            double error = output - this.Network.Run(input);
+
+            // Based on the error set new weights.
             for (int i = 0; i < this.Network.Inputs; i++)
             {
-                this.Network[i] += this.Alpha * a * input[i];
-                sum += a;
+                // Formula: new weight = old weight * alpha * input value
+                this.Network[i] += this.Alpha * error * input[i];
+                sum += error;
             }
             return sum;
         }
